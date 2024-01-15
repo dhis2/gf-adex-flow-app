43f501424a791753608cbaae623cf3336cebace8
NICOLA-HOBBY <150656866+NICOLA-HOBBY@users.noreply.github.com>
2024-01-15 14:23:01 -0500

    Update showExportLocalPackageWorkflow.js

--- src/views/showExportLocalPackageWorkflow.js
+++ src/views/showExportLocalPackageWorkflow.js
@@ -1,8 +1,8 @@
-import { ExportLocalPackage } from "../components/ExportLocalPackage.js";
-customElements.define("export-local-package", ExportLocalPackage);
+import { ExportLocalConfig } from "../components/ExportLocalConfig.js";
+customElements.define("export-local-config", ExportLocalConfig);
 
 /* global translator */
-export function showExportLocalPackageWorkflow() {
-    document.querySelector("#appContent").innerHTML = "<export-local-package></export-local-package><upgrade-status></upgrade-status>";
+export function showExportLocalConfigWorkflow() {
+    document.querySelector("#appContent").innerHTML = "<export-local-config></export-local-config><upgrade-status></upgrade-status>";
     translator.translatePageTo();
 }
