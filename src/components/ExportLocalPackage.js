5ff61e95fb91388091723a99770ed3db1b92afe0
NICOLA-HOBBY <150656866+NICOLA-HOBBY@users.noreply.github.com>
2024-01-15 14:20:19 -0500

    Update ExportLocalPackage.js

--- src/components/ExportLocalPackage.js
+++ src/components/ExportLocalPackage.js
@@ -1,8 +1,8 @@
 export class ExportLocalPackage extends HTMLElement {
     connectedCallback() {
-        this.innerHTML = ` <h2 data-i18n="export-local-package.title"></h2>
-        <p data-i18n="export-local-package.content"></p>
-        <button data-i18n="export-local-package.export-btn" onclick="exportLocalPackage()" id="btn-export-local-package"></button>`
+        this.innerHTML = ` <h2 data-i18n="export-local-config.title"></h2>
+        <p data-i18n="export-local-config.content"></p>
+        <button data-i18n="export-local-config.export-btn" onclick="exportLocalconfig()" id="btn-export-local-config"></button>`
         ;
     }
 }
